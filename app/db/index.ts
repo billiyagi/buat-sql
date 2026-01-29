3e401788e1d83572128d25e814235598f3d6ed74
Febry Billiyagi <febrybilliyagi72@gmail.com>
2026-01-29 10:21:43 +0700

    feat: Make database file path configurable via `DATABASE_FILE` environment variable and update Docker setup.

--- app/db/index.ts
+++ app/db/index.ts
@@ -2,5 +2,5 @@ import Database from "better-sqlite3";
 import { drizzle } from "drizzle-orm/better-sqlite3";
 import * as schema from "./schema";
 
-const sqlite = new Database("sqlite.db");
+const sqlite = new Database(process.env.DATABASE_FILE || "data/sqlite.db");
 export const db = drizzle(sqlite, { schema });
